a75cd8198d0a254b5c6a58164c166edcfc7d9e21
André d'Alva <andreluiscapela@gmail.com>
2025-03-06 11:56:36 +0100

    First Commit of the converter

---
+++ TemperatureConverter.js
@@ -0,0 +1,20 @@
+const fahrenheitConvert = document.getElementById("fahrenheitConvert");
+const celsiusConvert = document.getElementById("celsiusConvert");
+const displayResult = document.getElementById("results");
+
+let convertedTemperature;
+
+function converter(){
+    const givenTemperature = Number(document.getElementById("givenTemperature").value);
+
+    if(fahrenheitConvert.checked){
+        convertedTemperature = (givenTemperature*9/5) + 32+"ºF";
+    }
+    else if(celsiusConvert.checked){
+        convertedTemperature = (givenTemperature-32) * 5/9+"ºC";
+    }
+    else{
+        convertedTemperature = "Select an option!"
+    }
+    displayResult.textContent = convertedTemperature;
+}
