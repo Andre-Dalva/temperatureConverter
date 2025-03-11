ad7fa62a978317b2230105058ede770534f1193f
André d'Alva <andreluiscapela@gmail.com>
2025-03-11 11:29:02 +0100

    Accepting only number.

--- TemperatureConverter.js
+++ TemperatureConverter.js
@@ -7,14 +7,19 @@ let convertedTemperature;
 function converter(){
     const givenTemperature = Number(document.getElementById("givenTemperature").value);
 
-    if(fahrenheitConvert.checked){
-        convertedTemperature = (givenTemperature*9/5) + 32+"ºF";
-    }
-    else if(celsiusConvert.checked){
-        convertedTemperature = (givenTemperature-32) * 5/9+"ºC";
+    if(isNaN(givenTemperature)){
+        convertedTemperature = "Please give a number!!";
     }
     else{
-        convertedTemperature = "Select an option!"
+        if(fahrenheitConvert.checked){
+            convertedTemperature = (givenTemperature*9/5) + 32+"ºF";
+        }
+        else if(celsiusConvert.checked){
+            convertedTemperature = (givenTemperature-32) * 5/9+"ºC";
+        }
+        else{
+            convertedTemperature = "Select an option!"
+        }
     }
     displayResult.textContent = convertedTemperature;
 }
