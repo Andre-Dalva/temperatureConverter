310a8d1270f0e413722151d4b7f06d2fafbd2abf
André d'Alva <andreluiscapela@gmail.com>
2025-03-11 11:38:00 +0100

    Cleaning js file

--- TemperatureConverter.js
+++ TemperatureConverter.js
@@ -7,19 +7,12 @@ let convertedTemperature;
 function converter(){
     const givenTemperature = Number(document.getElementById("givenTemperature").value);
 
-    if(isNaN(givenTemperature)){
-        convertedTemperature = "Please give a number!!";
-    }
+    if(isNaN(givenTemperature)) convertedTemperature = "Please give a number!!";
+    
     else{
-        if(fahrenheitConvert.checked){
-            convertedTemperature = (givenTemperature*9/5) + 32+"ºF";
-        }
-        else if(celsiusConvert.checked){
-            convertedTemperature = (givenTemperature-32) * 5/9+"ºC";
-        }
-        else{
-            convertedTemperature = "Select an option!"
-        }
+        if(fahrenheitConvert.checked) convertedTemperature = (givenTemperature*9/5) + 32+"ºF";
+        else if(celsiusConvert.checked) convertedTemperature = (givenTemperature-32) * 5/9+"ºC";
+        else convertedTemperature = "Select an option!"
     }
     displayResult.textContent = convertedTemperature;
 }
